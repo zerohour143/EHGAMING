cfc1c74d5e0080aa494225984a0e57361b7aa6c8
zerohour143 <axeltantay@gmail.com>
2017-11-04 16:56:33 +0800

    optimization

--- Admin/Modules/functions/add-game.php
+++ Admin/Modules/functions/add-game.php
@@ -3,16 +3,15 @@
 
 	$title = $_POST['title'];
 	$details = $_POST['details'];
-	$target_dir = "../../../uploads/";
-	$target_dir2 = "../../uploads/";
+	$target_dir = "../../uploads/";
 	$filename = basename($_FILES["file"]["name"]);
 	$path = $target_dir . $filename;
 //CONNNNNNNNNNNNNNNNECTTTTTTTTTTTTTTTTTTT
 	$ops = new functions;
 
 //INSERRRRRRRTTTTTTTTTTTTTTTTTTTTTTTTTTTTT
-	if ($ops->insert_game($title,$details,$target_dir2 . $filename) === TRUE) {
-		move_uploaded_file($_FILES["file"]["tmp_name"], $path);
+	if ($ops->insert_game($title,$details,$path) === TRUE) {
+		move_uploaded_file($_FILES["file"]["tmp_name"], '../'.$path);
 	    echo '<script type="text/javascript">
 						window.location.href = "../game-dash.php";
 				</script>';
